c6d49e5cd6af4e85a669375c41eeed9ea45deaf4
Joan Odicio <yusuke.joan@gmail.com>
2025-07-30 23:40:12 +0200

    minor change to test only one deployment

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,10 @@ app.get("/health", (c) => {
   return c.json("ok", 200);
 });
 
+app.get("/health2", (c) => {
+  return c.json("ok2", 200);
+});
+
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
 
